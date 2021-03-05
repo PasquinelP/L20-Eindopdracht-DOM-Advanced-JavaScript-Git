d34cc50971e2a072f5fc29c35b5df5f3a096c973
Pascal Pater <pascal.pater@gmail.com>
2021-03-05 20:07:07 +0100

    Get all movie posters and attach to list in dom

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
+// Eindopdracht Pascal Pater
+// Javascript Advanced - Filmzoeker
+
+// check if database is accessible
 console.log(movies);
 
+// get all the needed elements and classes
 const movieList = document.querySelector("#movie-list");
 
-console.log(movieList);
-
 const addMoviesToDom = (movies) => {
-  
+  movieList.appendChild(movies);
 };
+
+// get the url for the poster of each movie from the movies database
+// create an li with an img element with the poster url as src
+// send the list of movie posters to addMoviesToDom function
+const getMoviePosters = movies.map((movie) => {
+  const moviePoster = movie.Poster
+  const movieItem = document.createElement("li");
+  const movieImg = document.createElement("img");
+  movieItem.appendChild(movieImg);
+  movieImg.src = moviePoster;
+  addMoviesToDom(movieItem);
+});
 
 
 
