6852418d6554df29760256d9e83ecf7aa2e0e1a2
Pascal Pater <pascal.pater@gmail.com>
2021-03-06 01:00:21 +0100

    make radiobuttons work on change, add movie img and link

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ console.log(movies);
 
 // get all the needed elements and classes
 const movieList = document.querySelector("#movie-list");
+const radioButtons = document.querySelectorAll("input[name='film-filter']");
 
+// add the movies to the movie list in the DOM
 const addMoviesToDom = (movies) => {
   movieList.appendChild(movies);
 };
@@ -18,10 +20,49 @@ const getMoviePosters = movies.map((movie) => {
   const moviePoster = movie.Poster
   const movieItem = document.createElement("li");
   const movieImg = document.createElement("img");
-  movieItem.appendChild(movieImg);
+  const movieLink = document.createElement("a");
+  movieItem.appendChild(movieLink).appendChild(movieImg);
+  movieID = movie.imdbID;
+  const link = "https://www.imdb.com/title/" + movieID;
+  movieLink.href = link;
+  movieLink.target = "_blank";
   movieImg.src = moviePoster;
   addMoviesToDom(movieItem);
 });
 
 
+// receive the selected element
+// get the value of the selected button
+const handleOnChangeEvent = (event) => {
+  console.log("dit is handleonchangeevent", event.target.value);
+  switch (event.target.value) {
+    case "latest":
+      console.log("hey ik ben Latest film");
+      break;
+    case "avenger":
+      console.log("hey ik ben Avneger film");
+      break;
+    case "xmen":
+      console.log("hey ik ben X-men film");
+      break;
+    case "princess":
+      console.log("hey ik ben Princess film");
+      break;
+    case "batman":
+      console.log("hey ik ben Batman film");
+      break;
+    default:
+      console.log("DEFAULT");
+  } 
+};
 
+// cycle through radio buttons
+// listen for a change so you know which radio button is selected
+// send selected element to handleOnchangeEvent function
+const addEventListeners = () => {
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", handleOnChangeEvent);
+});
+};
+
+addEventListeners();
