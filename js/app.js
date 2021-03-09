3d2336732e5e2c7ef29c8636d85bf12ca83c8f0d
Pascal Pater <pascal.pater@gmail.com>
2021-03-09 17:18:01 +0100

    update and clean up app.js, add styling, add img

--- js/app.js
+++ js/app.js
@@ -4,11 +4,22 @@
 // check if database is accessible
 console.log(movies);
 
-// get all the needed elements and classes
-const movieList = document.querySelector("#movie-list");
+// get the needed elements and classes
+const movieList = document.querySelector("#js-movie-list");
 const radioButtons = document.querySelectorAll("input[name='film-filter']");
 
+// get the url for the poster of each movie
+// create an li with an img element with the poster url as src
+const createImgElement = (moviePoster) => {
+  const movieImg = document.createElement("img");
+  movieImg.src = moviePoster;
+  return movieImg;
+};
 
+// construct the link to imdb
+// add the imdbID to the base url to create url
+// create a element and add url to href
+// add target to href to open in new page 
 const createMovieLink = (movieID) => {
   const link = "https://www.imdb.com/title/" + movieID;
   const movieLink = document.createElement("a");
@@ -17,126 +28,90 @@ const createMovieLink = (movieID) => {
   return movieLink;
 };
 
-const removeAllMovies = () => {
-  const movieListItems = document.querySelectorAll("#movie-list li");
+// filter movies on title based on value send from clicked button
+// put result in an array and send to addMoviesToDom
+const filterMovies = (wordInMovieTitle) => {
+  const getFilteredMovies = movies
+    .filter((movie) => movie.Title.includes(wordInMovieTitle))
+    .map((movie) => movie);
+  addMoviesToDom(getFilteredMovies);
+};
 
-  console.log("Movie list items: ", movieListItems);
+// filter movies based on year
+// put result in an array and send to addMoviesToDom
+const filterLatestMovies = () => {
+  const getLatestMovies = movies
+    .filter((movie) => movie.Year > 2013)
+    .map((movie) => movie);
+  addMoviesToDom(getLatestMovies);
+};
+
+// remove all movie items
+// so filtered items will not be added 
+// to the back of the previous list
+const removeAllMovies = () => {
+  const movieListItems = document.querySelectorAll("#js-movie-list li");
   movieListItems.forEach((movieListItem) => movieListItem.remove());
 };
 
-// add the movies to the movie list in the DOM
-const addMoviesToDom = (movies) => {
-  
-  removeAllMovies();
-
-  console.log("Ontvangen films", movies)
-  const movieItems = movies.map((movie) => {
-    const moviePoster = movie.Poster;
-    const movieID = movie.imdbID;
-    
-    const movieItem = document.createElement("li");
-    const movieImg = document.createElement("img");
-    const movieLink = createMovieLink(movieID);
-    // const movieLink = document.createElement("a");
-    movieItem.appendChild(movieLink).appendChild(movieImg);
-    
-    // const link = "https://www.imdb.com/title/" + movieID;
-    // movieLink.href = link;
-    // movieLink.target = "_blank";
-    movieImg.src = moviePoster;
-    return movieItem;
-  });
-
-  movieItems.forEach((movieItem) => {
-    movieList.appendChild(movieItem);
-  })
-  
-};
-
-addMoviesToDom(movies);
-
-
-
- 
-// createMovieLink(12345);
-
-// get the url for the poster of each movie from the movies database
-// create an li with an img element with the poster url as src
-// send the list of movie posters to addMoviesToDom function
-const getMoviePosters = movies.map((movie) => {
-  const moviePoster = movie.Poster
-  const movieItem = document.createElement("li");
-  const movieImg = document.createElement("img");
-  const movieLink = document.createElement("a");
-  movieItem.appendChild(movieLink).appendChild(movieImg);
-  movieID = movie.imdbID;
-  const link = "https://www.imdb.com/title/" + movieID;
-  movieLink.href = link;
-  movieLink.target = "_blank";
-  movieImg.src = moviePoster;
-  // addMoviesToDom(movieItem);
-});
-
-
-
 // receive the selected element
 // get the value of the selected button
+// send case to correct filter function
 const handleOnChangeEvent = (event) => {
-  console.log("dit is handleonchangeevent", event.target.value);
   switch (event.target.value) {
     case "latest":
-      console.log("hey ik ben Latest film");
       filterLatestMovies();
       break;
     case "avenger":
-      console.log("hey ik ben Avneger film");
       filterMovies("Avengers");
       break;
     case "xmen":
-      console.log("hey ik ben X-men film");
       filterMovies("X-Men");
       break;
     case "princess":
-      console.log("hey ik ben Princess film");
       filterMovies("Princess");
       break;
     case "batman":
-      console.log("hey ik ben Batman film");
       filterMovies("Batman");
       break;
     default:
       console.log("all");
-  } 
+  }
 };
 
 // cycle through radio buttons
 // listen for a change so you know which radio button is selected
 // send selected element to handleOnchangeEvent function
 const addEventListeners = () => {
-radioButtons.forEach((radioButton) => {
-  radioButton.addEventListener("change", handleOnChangeEvent);
-});
+  radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("change", handleOnChangeEvent);
+  });
 };
 
+// add the movies to the movie list in the DOM
+// first remove all existing movie items
+// loop through received movies by the filter
+// create listitem with poster img and link for each movie
+// add each movie item to the list in the DOM 
+const addMoviesToDom = (movies) => {
+  removeAllMovies();
+  const movieItems = movies.map((movie) => {
+    const moviePoster = movie.Poster;
+    const movieID = movie.imdbID;
+    const movieItem = document.createElement("li");
+    const movieImg = createImgElement(moviePoster);
+    const movieLink = createMovieLink(movieID);
+    movieItem.appendChild(movieLink).appendChild(movieImg);
+    return movieItem;
+  });
+
+  movieItems.forEach((movieItem) => {
+    movieList.appendChild(movieItem);
+  });
+};
+
+// call the event listeners for the radio buttons
 addEventListeners();
 
-
-
-const filterMovies = (wordInMovieTitle) => {
-  console.log("My value is", wordInMovieTitle);
-    const getFilteredMovies = movies
-      .filter((movie) => movie.Title.includes(wordInMovieTitle))
-      .map((movie) => movie);
-
-    addMoviesToDom(getFilteredMovies);
-};
-
-const filterLatestMovies = () => {
-  const getLatestMovies = movies
-  .filter((movie) => movie.Year > 2013)
-  .map((movie) => movie);
-
-  console.log("Layest movies: ", getLatestMovies);
-  addMoviesToDom(getLatestMovies);
-};
-
+// on first page load or reload show all movies
+addMoviesToDom(movies);
